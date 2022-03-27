fc0297c25d333d812647aeaf0cb151b06ebff0fe
valtok7 <morisoba2013@gmail.com>
2022-03-27 19:42:40 +0000

    tidy up

--- src/simple_udp.h
+++ src/simple_udp.h
@@ -14,19 +14,19 @@ private:
     const int BUFFER_MAX = 400;
 
 public:
-    simple_udp(std::string address, int port) {
+    simple_udp(std::string address, int port) : sock(0), addr{} {
         sock = socket(AF_INET, SOCK_DGRAM, 0);
         addr.sin_family = AF_INET;
         addr.sin_addr.s_addr = inet_addr(address.c_str());
         addr.sin_port = htons(port);
     }
     void udp_send(std::string word) {
-        sendto(sock, word.c_str(), word.length(), 0, (struct sockaddr *)&addr,
-               sizeof(addr));
+        sendto(sock, word.c_str(), word.length(), 0,
+               reinterpret_cast<struct sockaddr *>(&addr), sizeof(addr));
     }
 
     void udp_bind() {
-        bind(sock, (const struct sockaddr *)&addr, sizeof(addr));
+        bind(sock, reinterpret_cast<struct sockaddr *>(&addr), sizeof(addr));
     }
     std::string udp_recv() {
         char buf[BUFFER_MAX];
@@ -34,7 +34,7 @@ public:
         recv(sock, buf, sizeof(buf), 0);
         return std::string(buf);
     }
-    void udp_recv(char *buf, int size) {
+    void udp_recv(char *buf, int size) const {
         memset(buf, 0, size);
         recv(sock, buf, size, 0);
     }
