f7d94492da854edde63e561641daf0ceb2b52a9b
valtok7 <morisoba2013@gmail.com>
2022-03-28 20:12:22 +0000

    python

--- .devcontainer/run_docker_env.sh
+++ .devcontainer/run_docker_env.sh
@@ -1,2 +1,2 @@
 sudo docker image build -t docker_env .
-sudo docker run -v /home/ito/work/docker-env:/workspace/docker_env -w /workspace/docker_env -it docker_env
+sudo docker run --privileged -v /home/ito/work/docker-env:/workspace/docker_env -w /workspace/docker_env -it docker_env
