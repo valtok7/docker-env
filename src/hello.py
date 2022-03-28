4cf9b98ed19377b0b66a3f32e94f0dc09476fd1c
valtok7 <morisoba2013@gmail.com>
2022-03-28 20:58:53 +0000

    drawrect

--- src/hello.py
+++ src/hello.py
@@ -1,1 +1,16 @@
-print("I'm Python!!")
+# リストa 内の位置 (x,y) に大きさ (w,h) の矩形を描く。
+def drawrect(a, x, y, w, h):
+    for px in range(x, x+w):
+        a[px][y] = 1
+        a[px][y+h-1] = 1
+    for py in range(y, y+h):
+        a[x][py] = 1
+        a[x+w-1][py] = 1
+    return
+
+
+canvas = [[0]*10 for i in range(10)]
+drawrect(canvas, 0, 0, 10, 10)
+drawrect(canvas, 2, 3, 6, 4)
+for line in canvas:
+    print(line)
