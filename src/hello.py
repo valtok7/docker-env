0f95679f3c08314e4c91fa8089e8e6ff786dcd61
valtok7 <morisoba2013@gmail.com>
2022-03-29 20:33:48 +0000

    Exception

--- src/hello.py
+++ src/hello.py
@@ -14,3 +14,45 @@ drawrect(canvas, 0, 0, 10, 10)
 drawrect(canvas, 2, 3, 6, 4)
 for line in canvas:
     print(line)
+
+# List (mutable)
+print("List")
+list_a = [1, 2, 3, 4]
+for list_a_it in list_a:
+    print(list_a_it)
+
+# Tuple (imutable)
+print("Tuple")
+tuple_a = (1, 2, 3, 4)
+for tuple_a_it in tuple_a:
+    print(tuple_a_it)
+
+# Dictionary
+print("Dictionary")
+dict_a = {"abc": 1, "def": 2, "ghi": 1}
+for dict_a_it in dict_a:
+    print(dict_a_it)
+    print(dict_a[dict_a_it])
+
+# Set
+print("Set")
+set_a = {"abc", "def", "ghi"}
+for set_a_it in set_a:
+    print(set_a_it)
+
+# Function
+print("Function")
+
+
+def add(x, y):
+    return x+y
+
+
+print(add(1, 2))
+
+# Exception
+print("Exception")
+try:
+    print("a" / 5)
+except Exception as e:
+    print("Exception occured!")
